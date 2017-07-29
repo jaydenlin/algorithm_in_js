39e2cd246248eae112be28056b161abb82222ef9
jayden <jayden@yahoo-inc.com>
2017-07-30 01:57:38 +0800

    cut stick

--- dp/cut_stick_top_down.js
+++ dp/cut_stick_top_down.js
@@ -2,7 +2,7 @@ var value = [null, 1, 5, 8, 9];//value[1]=1;代表一個單位價值1，value[2]
 var n = 4;
 var dp=[0, -1, -1, -1, -1];
 dp[0]=0;//dp[1] = 1 代表當n=1時，切後價值最大為1
-//max(value[i], stick(n - i) + value[i])
+//stick(n) = max(value[i], stick(n - i) + value[i])
 var stick = function(L){
     if(dp[L]!==-1){ return dp[L]}
     dp[L]=0;
@@ -15,4 +15,4 @@ var stick = function(L){
 };
 stick(n);
 console.log(dp[n]);
-//top down 從dp[n]開始往下算
+//top down 從stick(n)開始往下算
